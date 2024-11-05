0e6c39bd5dc8bfecf5d417bb969c49db032dc396
janekpiotrowski123 <165053502+janekpiotrowski123@users.noreply.github.com>
2024-11-05 13:04:31 +0100

    finalne

--- servers_skeleton.py
+++ servers_skeleton.py
@@ -1,4 +1,6 @@
-from typing import Optional
+from abc import abstractmethod, ABC
+from itertools import product
+from typing import Optional, List, Dict, Union
 
 
 class Product:
@@ -12,6 +14,8 @@ class Product:
         self.price=cena
 
     def __eq__(self, other):
+        if not isinstance(other, Product):
+            return False
         if self.name == other.name and self.price== other.price :
             return True
         return False 
@@ -19,7 +23,7 @@ class Product:
     def __hash__(self):
         return hash((self.name, self.price))
     
-class TooManyProductsFoundError:
+class TooManyProductsFoundError(Exception):
     # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
     pass
 
@@ -29,11 +33,47 @@ class TooManyProductsFoundError:
 #   (2) możliwość odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę wyników wyszukiwania,
 #   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
 
-class ListServer:
+class Server(ABC):
+    n_max_returned_entries = 3
+
+    @abstractmethod
+    def get_entries(self, n_letters = 1):
+        pass
+
+class ListServer(Server):
+    def __init__(self, products: List[Product]):
+        self.products = products
+
+    def get_entries(self, n_letters = 1) -> List[Product]:
+        result = []
+        for product in self.products:
+            if product.name[:n_letters].isalpha() and product.name[n_letters:].isdigit() and (len(product.name) == 2 + n_letters or len(product.name) == n_letters + 3):
+                result.append(product)
+        if len(result) > self.n_max_returned_entries:
+            raise TooManyProductsFoundError()
+
+        result.sort(key = lambda x: x.price)
+        return result
     pass
 
 
-class MapServer:
+class MapServer(Server):
+
+    def __init__(self, products: List[Product]):
+        products = {p.name : p for p in products}
+        self.products = products
+
+    def get_entries(self, n_letters = 1) -> List[Product]:
+        result = []
+        for name, product  in self.products.items():
+            if name[:n_letters].isalpha() and name[n_letters:].isdigit() and (len(name) == 2 + n_letters or len(name) == n_letters + 3):
+                result.append(product)
+        if len(result) > self.n_max_returned_entries:
+            raise TooManyProductsFoundError()
+
+        result.sort(key = lambda x: x.price)
+        return result
+
     pass
 
 
@@ -42,7 +82,7 @@ class MapServer:
 class Client:
      #FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą obiekt reprezentujący serwer
 
-    def __init__(self, server) -> None:
+    def __init__(self, server: Server) -> None:
         if not hasattr(server, "get_entries"):
             raise ValueError("Dostarczony obiekt serwera nie posiada metody „get_entries”.")
         self.server = server
